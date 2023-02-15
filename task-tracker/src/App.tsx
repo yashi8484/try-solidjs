7653eb5f73830254503b9da55e5ae3c00e8a9a87
yashi8484 <kobayashi@giftee.co>
2023-02-15 19:06:19 +0900

    Add features to add / delete tasks

--- task-tracker/src/App.tsx
+++ task-tracker/src/App.tsx
@@ -1,25 +1,58 @@
-import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import { Component, createSignal, For } from 'solid-js';
 
 const App: Component = () => {
+  type Task = {
+    id: string;
+    text: string;
+    completed: boolean;
+  }
+
+  const [taskList, setTaskList] = createSignal([] as Task[]);
+
+  const addTask = (e: Event) => {
+    // prevent the default reload behavior when we submit our form.
+    e.preventDefault();
+
+    const taskInput = document.querySelector("#taskInput") as HTMLInputElement;
+
+    const newTask: Task = {
+      id: Math.random().toString(36).substring(2),
+      text: taskInput.value,
+      completed: false,
+    }
+
+    setTaskList([newTask, ...taskList()]);
+    taskInput.value = '';
+  }
+
+  const deleteTask = (taskId: string) => {
+    const newTaskList = taskList().filter((task) => task.id !== taskId);
+    setTaskList(newTaskList);
+  }
+
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div class="container mt-5 text-center">
+      <h1 class="mb-4">Whattodo!</h1>
+
+      <form class="mb-5 row row-cols-2 justify-content-center" onSubmit={(e) => addTask(e)}>
+        <input type="text" class="input-group-text p-1 w-25" placeholder="Add task here..." id="taskInput" required />
+
+        <button class="btn btn-primary ms-3 w-auto" type="submit">
+          Add task
+        </button>
+      </form>
+      <div>
+        <h4 class="text-muted mb-4">Tasks</h4>
+        <For each={taskList()}>
+          {(task: Task) => (
+            <div class="row row-cols-3 mb-3 justify-content-center">
+              <button class="btn btn-danger w-auto" onClick={() => deleteTask(task.id)}>X</button>
+              <div class="bg-light p-2 mx-2">{task.text}</div>
+              <input type="checkbox" checked={task.completed} role="button" class="form-check-input h-auto px-3" />
+            </div>
+          )}
+        </For>
+      </div>
     </div>
   );
 };
